fb2add9080b838d93af4144f5a18b0b43fd80c1c
LesleyMdCampo <lesliz32@gmail.com>
2014-01-10 15:59:56 -0600

    Create associations for post model.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,6 @@
 class Trip < ActiveRecord::Base
   belongs_to :user
   belongs_to :destination
+
+  has_many :posts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   has_many :trips
   has_many :destinations, through: :trips
-  
+  has_many :posts
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
