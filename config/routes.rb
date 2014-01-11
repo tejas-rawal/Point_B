84b035832b2a5689f9084960c0f534c187075f80
LesleyMdCampo <lesliz32@gmail.com>
2014-01-11 12:19:34 -0600

    Move destination routes to bottom of file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 PointB::Application.routes.draw do
   
-
-  resources :destinations, only: :show
-
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   resources :users do 
     resources :trips
   end
+  
+  resources :destinations, only: :show
 
   root 'static_pages#home'
 
