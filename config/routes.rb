88e3bd8a625fe616d60f15b6d915c77645fb0b3c
LesleyMdCampo <lesliz32@gmail.com>
2014-01-10 19:36:38 -0600

    Edit logout in devise.rb. Change defualt sign-in routes to be /login and /logout.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PointB::Application.routes.draw do
 
   resources :destinations, only: :show
 
-  devise_for :users
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
   resources :users
 
