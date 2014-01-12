7913448a02e20737a56f0089e5a0b916e0e0e4ce
Tejas Rawal <trawal90@gmail.com>
2014-01-11 19:27:00 -0600

    Add routes for ajax post request on post.

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ PointB::Application.routes.draw do
   
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
 
-
+  scope :api do
+    post 'destinations/:id/posts' => 'destinations#posts', :as => :destination_post_api
+    
   resources :users do 
     resources :destinations
   end
-  
-  
 
   root 'static_pages#home'
 
