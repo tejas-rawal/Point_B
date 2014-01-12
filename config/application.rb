1e3fab55bc921ce0f0947ee52b7ed3c90d4b1124
LesleyMdCampo <lesliz32@gmail.com>
2014-01-11 21:12:22 -0600

    Add new files for css types. Correct grid for destinations on user#show.

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module PointB
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # add app/assets/fonts to the asset path
   end
 end
