cd84f3a452b115ae423cfa23d523c74d71f81d5d
Tejas Rawal <trawal90@gmail.com>
2014-01-12 17:36:11 -0600

    Add additional css to destination show page.

--- app/assets/javascripts/app/presenters/post-wall-presenter.js
+++ app/assets/javascripts/app/presenters/post-wall-presenter.js
@@ -10,7 +10,7 @@
     posts.on('create', function (newPost) {
       var newHtml = $.render(postTemplate, newPost);
       console.log(newPost);
-      $root.append(newHtml).slideDown();
+      $root.append(newHtml).slideDown(newHtml);
     });
   };
 })();
