50f9089885c423db41b893e7a35f78eb51161834
Tejas Rawal <trawal90@gmail.com>
2014-01-15 00:28:37 -0600

    Add user model method to get lat and lng of user destination.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def getLatLng
+    self.each do |destination|
+    response = Unirest.get("http://maps.googleapis.com/maps/api/geocode/json?address=#{destination.city}&sensor=false")
+    lat = response.body['results'][0]['geometry']['location']['lat']
+    lng = response.body['results'][0]['geometry']['location']['lng']
+    end
+  end
 end
