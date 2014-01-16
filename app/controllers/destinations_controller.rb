43657620e21166a81b4ec39a33e1d3deda080a62
LesleyMdCampo <lesliz32@gmail.com>
2014-01-16 13:14:23 -0600

    Corrected destination controller to correct commit.

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,40 +1,30 @@
-class DestinationsController < ApplicationController
-  before_action :set_destination, only: [:show, :edit, :update, :destroy]
+class CommentsController < ApplicationController
 
-  # GET /trips
-  # GET /trips.json
+  before_action :set_comment, only: [:show, :edit, :update, :destroy]
+
+  # GET /comments
+  # GET /comments.json
   def index
-    @destinations = Destination.all
+    @comments = Comment.all
   end
 
-  # GET /destinations/1
-  # GET /destinations/1.json
+  # GET /comments/1
+  # GET /comments/1.json
   def show
-    @user = User.find(params[:user_id])
-    @post = @destination.posts(post_params)
-    @comment = Comment.new
   end
 
-  def post
-    current_user
-    @destination = current_user.destinations.find(params[:destination_id])
-    new_post = @destination.posts.create(post_params)
-    new_post.destination_id = params[:destination_id]
-    render :json => new_post.to_json, :status => 200
-  end
-
-  # GET /destinations/new
+  # GET /comments/new
   def new
-    @destination = Destination.new
-    @user = User.find(params[:user_id])
+    @comment = Comment.new
+    @post = Post.find(params[:post_id])
   end
 
-  # GET /destinations/1/edit
+  # GET /comments/1/edit
   def edit
   end
 
-  # POST /destinations
-  # POST /destinations.json
+  # comment /comments
+  # comment /comments.json
   def create
     post = Post.find(params[:comment][:post_id])
     destination = post.destination
@@ -49,44 +39,38 @@ class DestinationsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /destinations/1
-  # PATCH/PUT /destinations/1.json
+  # PATCH/PUT /comments/1
+  # PATCH/PUT /comments/1.json
   def update
-    @user = User.find(params[:user_id])
     respond_to do |format|
-      if @destination.update(destination_params)
-        format.html { redirect_to user_destination_path(@user.id, @destination.id), notice: 'destination was successfully updated.' }
+      if @comment.update(comment_params)
+        format.html { redirect_to @destination, notice: 'comment was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @destination.errors, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /destinations/1
-  # DELETE /destinations/1.json
+  # DELETE /comments/1
+  # DELETE /comments/1.json
   def destroy
-    @destination.destroy
-    @user = User.find(params[:user_id])
+    @comment.destroy
     respond_to do |format|
-      format.html { redirect_to user_path(@user) }
+      format.html { redirect_to destination_url }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_destination
-      @destination = Destination.find(params[:id])
+    def set_comment
+      @comment = Comment.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def destination_params
-      params.require(:destination).permit(:city, :country, :description, :album, :category, :user_id, :post_id)
-    end
-
-    def post_params
-      params.permit(:activity, :description, :external_links, :images, :destination_id)
+    def comment_params
+      params.require(:comment).permit(:text, :user_id, :post_id, :destination_id)
     end
 end
