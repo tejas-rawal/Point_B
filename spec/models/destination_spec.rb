47d9947b009345b61e23256e8b1415f279e630a3
Tejas Rawal <trawal90@gmail.com>
2014-01-17 01:30:25 -0600

    Add rspec test for lat_lng method in destination model which passes.

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Destination do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:destination) { Destination.create(city: 'New York', country: 'United States', category: 'volunteer')}
+
+  describe '#lat_lng' do
+    context 'when given :city and :country for destination' do
+      it 'gives back latitude and longitude coordinates' do
+        expect(destination.lat_lng).to eq({lat: 40.7143528, lng: -74.00597309999999})
+      end
+    end
+  end
 end
