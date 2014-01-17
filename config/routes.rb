7212ad1187a05f78826c9a1ec88c628997fe01eb
LesleyMdCampo <lesliz32@gmail.com>
2014-01-17 15:36:12 -0600

    Remove dup devise route.

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ PointB::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  devise_scope :user do
-   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
-  end
-
   resources :users do
     resources :destinations
   end
