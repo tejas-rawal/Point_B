c1311904425960d30c3005541610076b98f9e31d
LesleyMdCampo <lesliz32@gmail.com>
2014-01-17 15:42:35 -0600

    Adjustments to user.rb to get fb working hopefully.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,23 +10,23 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
 
-  def self.find_for_facebook_oauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      user.save!
-    end
-  end
-
-  def self.new_with_session(params, session)
-    super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
+  def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+      if user
+        return user
+      else
+        registered_user = User.where(:email => auth.info.email).first
+        if registered_user
+          return registered_user
+        else
+          user = User.create(name:auth.extra.raw_info.name,
+            provider:auth.provider,
+            uid:auth.uid,
+            profile_pic:auth.info.image,
+            email:auth.info.email,
+            password:Devise.friendly_token[0,20],
+          )
+      end    
     end
   end
 
