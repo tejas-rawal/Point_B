2bb91a845e413a5c4de43d36c853bee34a74f24b
LesleyMdCampo <lesliz32@gmail.com>
2014-01-17 15:35:19 -0600

    Add OmniauthCallback controller with controller and  model methds. Add devise routes for fb.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ PointB::Application.routes.draw do
 
   resources :comments
 
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  # devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   resources :users do
     resources :destinations
