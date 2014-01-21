ec7e3a4d4a42221ab486ee23dd166b40d731bc43
LesleyMdCampo <lesliz32@gmail.com>
2014-01-21 09:38:58 -0600

    Add current user method and hide action to applications controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
+
   include PublicActivity::StoreController
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
@@ -10,9 +11,11 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
-# ...
-hide_action :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
+  hide_action :current_user
 
   def after_sign_in_path_for(user)
     params[:target] || user_path(user.id)
