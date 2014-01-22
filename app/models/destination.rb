0ca2eeebba758140999f6d39a85ca38fbb711680
Tejas Rawal <trawal90@gmail.com>
2014-01-22 17:18:57 -0600

    Add link to user name for each destination. Optimize search query for cities.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -27,7 +27,7 @@ class Destination < ActiveRecord::Base
 
   def self.search(search)
     if search
-      find(:all, :conditions => ['city LIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['city LIKE ?', "%#{search.split.map(&:capitalize).join(' ')}%"])
     else
       find(:all)
     end
