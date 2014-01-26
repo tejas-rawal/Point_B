ee13d1628dc88ce4212b6cdf9c2d0dca1b347eee
LesleyMdCampo <lesliz32@gmail.com>
2014-01-26 14:44:02 -0600

    Change method to patch for feed.

--- app/assets/javascripts/activities.js
+++ app/assets/javascripts/activities.js
@@ -5,17 +5,18 @@
   });
 
   function updateFeed(){
-      $.ajax({
-          url: '/activities.json'+decodeURIComponent(JSON.stringify({"json":[{"j":"son"}]})),
-          contentType: "application/json; charset=utf-8",
-          dataType: "json",
-          success: function(data){
-              $('.activity').html(data);
-          },
-          error: function(){
-              clearInterval(interval); // stop the interval
-              $('.activity').html('<span style="color:red">Connection problems</span>');
-          }
-      });
+    $.ajax({
+        method: 'PATCH',
+        url: '/activities.json',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function(data){
+            $('.activity').html(data);
+        },
+        error: function(){
+            clearInterval(interval); 
+            $('.activity').html('<span style="color:red">Connection problems</span>');
+        }
+    });
   }
 })();
