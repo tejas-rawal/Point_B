5357ad76d5e2e159e30d9c423f7cc888152123e1
Tejas Rawal <trawal90@gmail.com>
2014-04-02 04:07:50 -0500

    Add tests for #country_code in destination model.

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -5,14 +5,24 @@ describe Destination do
   let(:destination2) { Destination.create(city: 'Austin', country: 'United States', category: 'adventure')}
 
   describe '#snippet' do
-    context ' when provided with a long description for the destination' do
+    context 'when provided with a long description for the destination' do
       it 'truncates the text to 200 characters' do
-        description = 'New York, New York: a city so nice they named it twice, and a city so colorful it has stood at the forefront of the American imagination for decades. Artists, filmmakers, intellectuals, designers, and more have long tried to capture the essence of the city, fetishizing its skyline, dramatizing its streets, and immortalizing the diversity of people that call the five boroughs home.'
-        expect(description.snippet.length).to eq 200
+        destination.description = 'New York, New York: a city so nice they named it twice, and a city so colorful it has stood at the forefront of the American imagination for decades. Artists, filmmakers, intellectuals, designers, and more have long tried to capture the essence of the city, fetishizing its skyline, dramatizing its streets, and immortalizing the diversity of people that call the five boroughs home.'
+        expect(destination.snippet.length).to eq 200
 
-        description2 = 'Austin is a city and capital of the U.S. state of Texas. Austin has a population of 681,804 people, making it the fourth-largest city in Texas and the 16th largest in the United States. Austin is the county seat of Travis County and is situated in Central Texas. The Austin—Round Rock metropolitan area, a title designated by the U.S. Census, is one of the fastest-growing in the United States and is home to an estimated population of 1,377,633 in 2003.'
-        expect(description2.snippet.length).to eq 200
+        destination2.description = 'Austin is a city and capital of the U.S. state of Texas. Austin has a population of 681,804 people, making it the fourth-largest city in Texas and the 16th largest in the United States. Austin is the county seat of Travis County and is situated in Central Texas. The Austin—Round Rock metropolitan area, a title designated by the U.S. Census, is one of the fastest-growing in the United States and is home to an estimated population of 1,377,633 in 2003.'
+        expect(destination2.snippet.length).to eq 200
       end
+    end
+  end
+
+  describe '#country_code' do
+    context'when provided with the country of a specific destination' do
+      it 'returns the international country code for that country'
+      expect(destination.country_code).to eq 'us'
+
+      destination3 = Destination.create(city: 'Madrid', country: 'Spain')
+      expect(destination3.country_code).to eq ''
     end
   end
 
@@ -20,7 +30,7 @@ describe Destination do
     context 'when given :city and :country for destination' do
       it 'gives back latitude and longitude coordinates' do
         expect(destination.lat_lng).to eq({lat: 40.7143528, lng: -74.00597309999999})
-        expect(destination2.lat_lng).to eq({lat: 30.25000, lng: -97.7500})
+        expect(destination2.lat_lng).to eq({lat: 30.267153, lng: -97.7430608})
       end
     end
   end
